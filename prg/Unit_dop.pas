68913ee2b1b17ea5eddb7e788930a528cce2d769
TarasovDV-rep <79887887+TarasovDV-rep@users.noreply.github.com>
2021-03-02 06:50:21 +0300

    Add files via upload
    Загрузка в репозитроий каталога d:\IVClient\prg

---
+++ prg/Unit_dop.pas
@@ -0,0 +1,20 @@
+unit Unit_dop;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,db,shellapi,
+  Dialogs, ComCtrls, ToolWin, ExtCtrls, ImgList, kbmMemTable, GridsEh, DBGridEh,Unit_new_conn,
+  Menus;
+
+var
+  t : TkbmMemTable;
+  ds : TDataSource;
+  posl_shell : Integer;
+  name_new_usel : String;
+  im : String;
+
+
+implementation
+
+end.
